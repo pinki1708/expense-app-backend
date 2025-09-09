c2cba73a94c2202ed1050d69d9b58afb03d2316f
pinki1708 <147258122+pinki1708@users.noreply.github.com>
2025-09-09 22:43:28 +0530

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('expense_app_db', 'root', 'pinki', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false
+});
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+testConnection();
+
+module.exports = sequelize;
